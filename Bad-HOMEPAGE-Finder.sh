6c397b3f07afef12ff22c2f5e07517f41b70bcb5
David <write2david@gmail.com>
2011-06-07 00:53:31 -0500

    Many display/formatting updates

--- Bad-HOMEPAGE-Finder.sh
+++ Bad-HOMEPAGE-Finder.sh
@@ -13,13 +13,13 @@ echo
 echo 
 echo "Welcome to the Portage Tree Homepage Checker"
 echo 
+echo "   The current version is available at:"
 echo "   https://github.com/write2david/Portage-HOMEPAGE-Checker"
 echo
 echo
-echo "This script tests all the HOMEPAGE variables in all the ebuilds in the Portage tree."
-echo "For that HOMEPAGE URL\'s that *don\'t* have a \'200\' (OK) or \'302\' (redirect) HTTP status code,"
-echo  "     this script displays that URL, what the status code is, and which ebuilds use it."
-echo  "     Then bugs can be filed on bugs.gentoo.org so that the HOMEPAGE can be updated."
+echo "We will now test all HOMEPAGE variables in the Portage tree."
+echo "     We'll find those that have DNS issues.  We'll also find those"
+echo "     without a '200' (OK) or '302' (redirect) HTTP status code."
 echo
 echo
 
@@ -37,18 +37,20 @@ echo
 
 echo
 echo
-echo
-echo "Getting list of all HOMEPAGE's in the Portage Tree (this will take a few moments)..."
+echo "1) Getting list of all HOMEPAGE's in the Portage Tree."
+echo "     (this will take a few moments)..."
 echo
 
 # Homepages may be commented out, so in the following line, we grep for HOMEPAGE only if it is at the beginning of the line.  See http://bugs.gentoo.org/show_bug.cgi?id=366957
 
-find /usr/portage/ -name '*.ebuild' -type f -exec grep '^HOMEPAGE' '{}' \; | sed 's/HOMEPAGE=//' | sed 's/"//g' | sed 's/\ /\n/g' | grep http:// > /tmp/PortageHomepages-Unsorted.txt
+/usr/bin/time -p -o /tmp/PTHC-Find-Homepages.txt find /usr/portage/ -name '*.ebuild' -type f -exec grep '^HOMEPAGE' '{}' \; | sed 's/HOMEPAGE=//' | sed 's/"//g' | sed 's/\ /\n/g' | grep http:// > /tmp/PortageHomepages-Unsorted.txt
 
 sort /tmp/PortageHomepages-Unsorted.txt | uniq > /tmp/PortageHomepages.txt
 
-echo "     Number of unique HOMEPAGE URL's:"
-wc -l /tmp/PortageHomepages.txt | awk '{ print "     "$1}'
+echo "     Number of unique HOMEPAGE URL's: `wc -l /tmp/PortageHomepages.txt | awk '{ print "     "$1}'`"
+echo
+echo "     Amount of time this step took: `cat /tmp/PTHC-Find-Homepages.txt | head -n 1 | awk '{ print $2}'`"
+echo
 echo
 echo
 
@@ -57,13 +59,23 @@ echo
 
 # check all the sites (with "wget --spider") to see if the URL's are either good or broken
 
-echo "Checking each HOMEPAGE in the Portage tree to see if it is accessible..."
+echo "2) Checking each HOMEPAGE in the Portage tree to see if it is accessible..."
 echo
-echo "  This process will take a little while."
-echo "  You can monitor the progress by running this command in another terminal:  tail -f /tmp/PortageHomepagesTested.txt"
+echo "     This process will take a while."
+echo "     You can monitor the progress by running this command in another terminal:"
+echo "        tail -f /tmp/PortageHomepagesTested.txt"
+echo
 
-wget --spider -nv -i /tmp/PortageHomepages.txt -o /tmp/PortageHomepagesTested.txt --timeout=10 --tries=3 --waitretry=10 --no-check-certificate --no-cookies
+/usr/bin/time -p -o /tmp/PTHC-Check-Each-Homepage.txt wget --spider -nv -i /tmp/PortageHomepages.txt -o /tmp/PortageHomepagesTested.txt --timeout=10 --tries=3 --waitretry=10 --no-check-certificate --no-cookies
+echo
+echo "     Amount of time this step took: `cat /tmp/PTHC-Check-Each-Homepage.txt | head -n 1 | awk '{ print $2}'`"
 
+
+echo
+echo
+echo "3) Processing the results..."
+echo
+echo
 
 
 # STEP 2.5
@@ -71,7 +83,6 @@ wget --spider -nv -i /tmp/PortageHomepages.txt -o /tmp/PortageHomepagesTested.tx
 # GO through the results of the previous command and get all the DNS resolution errors (which is something the next step doesn't catch)
 
 grep 'unable to resolve host address' /tmp/PortageHomepagesTested.txt > /tmp/RealPortageHomepageIssues-DNS-ISSUES.txt
-cat /tmp/PortageHomepageIssues-DNS-ISSUES.txt
 
 # pull only the URL (the 7th field), then get only the characters a-z, A-Z, 0-9, and period (that is, remove the quotes)
 cat /tmp/PortageHomepageIssues-DNS-ISSUES.txt | awk '{ print $7}' | sed s/[^a-zA-Z0-9.]//g > /tmp/RealPortageHomepageIssues-DNS-ISSUES.txt 
@@ -90,7 +101,7 @@ grep -v '200 OK' /tmp/PortageHomepagesTested.txt | grep -v '200 Ok' | grep http 
 
 # Get the HTTP codes all the HOMEPAGES that have issues
 
-echo "Removing redirects (302) from the list of \"broken\" HOMEPAGE's..."
+# Removing redirects (302) from the list of "broken" HOMEPAGE's
 
 wget -i /tmp/PortageHomepagesWithIssues.txt -o /tmp/PortageHomepagesLogs.txt -O /tmp/PortageHomepageDump
 
@@ -112,7 +123,6 @@ cat /tmp/PortageHomepagesWith302.txt >> /tmp/PortageHomepagesWithIssues.txt
 
 sort /tmp/PortageHomepagesWithIssues.txt | uniq -u > /tmp/RealPortageHomepageIssues.txt
 
-rm /tmp/PortageHome*
 
 
 
@@ -120,22 +130,26 @@ rm /tmp/PortageHome*
 
 # Display the useful information.
 
-echo "There are `wc -l /tmp/RealPortageHomepageIssues-DNS-ISSUES.txt | awk '{print $1}'` HOMEPAGES that have DNS issues."
+echo
+echo "4) Producing Results..."
+echo
+echo
+echo "     There are `wc -l /tmp/RealPortageHomepageIssues-DNS-ISSUES.txt | awk '{print $1}'` HOMEPAGES that have DNS issues."
 echo ""
 echo ""
-echo "Here they are..."
+echo "     They are being recorded in /tmp/PTHC-Bad-DNS-Homepages.txt..."
 echo 
-
+rm -f /tmp/PTHC-Bad-DNS-Homepages.txt > /dev/null
 
 for i in $(cat /tmp/RealPortageHomepageIssues-DNS-ISSUES.txt) ; do
-echo ""
-echo "HOMEPAGE with a problem...    $i"
-echo "   ...Searching Portage tree for all ebuilds that use this HOMEPAGE."
-echo "   (need to fix: currently, all ebuilds that reference this URL will be included, whether or not it is the HOMEPAGE variable or the SRC_URI variable.)" 
-echo ""
-find /usr/portage/ -name '*.ebuild' -type f -print | xargs -i grep -l $i {}
-echo ""
-echo ""
+echo "" >> /tmp/PTHC-Bad-DNS-Homepages.txt
+echo "HOMEPAGE with a problem...    $i" >> /tmp/PTHC-Bad-DNS-Homepages.txt
+echo "   ...Searching Portage tree for all ebuilds that use this HOMEPAGE." >> /tmp/PTHC-Bad-DNS-Homepages.txt
+echo "   (need to fix: currently, all ebuilds that reference this URL will be included, whether or not it is the HOMEPAGE variable or the SRC_URI variable.)" >> /tmp/PTHC-Bad-DNS-Homepages.txt 
+echo "" >> /tmp/PTHC-Bad-DNS-Homepages.txt
+find /usr/portage/ -name '*.ebuild' -type f -print | xargs -i grep -l $i {} >> /tmp/PTHC-Bad-DNS-Homepages.txt
+echo "" >> /tmp/PTHC-Bad-DNS-Homepages.txt
+echo "" >> /tmp/PTHC-Bad-DNS-Homepages.txt
 done
 
 
@@ -144,25 +158,25 @@ done
 echo
 echo
 echo
-echo "There are `wc -l /tmp/RealPortageHomepageIssues.txt | awk '{print $1}'` HOMEPAGES that have other issues."
+echo "     There are `wc -l /tmp/RealPortageHomepageIssues.txt | awk '{print $1}'` HOMEPAGES that have other issues."
 echo ""
 echo ""
-echo "Here they are..."
+echo "     They are being recorded in /tmp/PTHC-Missing-Homepages.txt"
 echo 
-
+rm -f /tmp/PTHC-Missing-Homepages.txt > /dev/null
 
 for i in $(cat /tmp/RealPortageHomepageIssues.txt) ; do
 echo ""
-echo "HOMEPAGE with a problem...    $i"
+echo "HOMEPAGE with a problem...    $i" >> /tmp/PTHC-Missing-Homepages.txt
 wget $i -o /tmp/RealProblemLog.txt -O /tmp/RealProblemDump
 tail -n2 /tmp/RealProblemLog.txt | head -n1 | awk '{ print $4 " " $5 " "$6 " " $7 " " $8 " " $9}' > /tmp/RealProblemCode.txt
-echo "   ...Type of problem with this URL...   `cat /tmp/RealProblemCode.txt`"
-echo "   ...Searching Portage tree for all ebuilds that use this HOMEPAGE." 
-echo "   (need to fix: currently, all ebuilds that reference this URL will be included, whether or not it is the HOMEPAGE variable or the SRC_URI variable.)" 
-echo ""
-find /usr/portage/ -name '*.ebuild' -type f -print | xargs -i grep -l $i {}
-echo ""
-echo ""
+echo "   ...Type of problem with this URL...   `cat /tmp/RealProblemCode.txt`" >> /tmp/PTHC-Missing-Homepages.txt
+echo "   ...Searching Portage tree for all ebuilds that use this HOMEPAGE."  >> /tmp/PTHC-Missing-Homepages.txt
+echo "   (need to fix: currently, all ebuilds that reference this URL will be included, whether or not it is the HOMEPAGE variable or the SRC_URI variable.)" >> /tmp/PTHC-Missing-Homepages.txt 
+echo "" >> /tmp/PTHC-Missing-Homepages.txt
+find /usr/portage/ -name '*.ebuild' -type f -print | xargs -i grep -l $i {} >> /tmp/PTHC-Missing-Homepages.txt
+echo "" >> /tmp/PTHC-Missing-Homepages.txt
+echo "" >> /tmp/PTHC-Missing-Homepages.txt
 # Remove temporary files that wget produced in this "for/done" section:
 rm /tmp/RealProblemLog.txt
 rm /tmp/RealProblemDump
@@ -170,18 +184,20 @@ done
 
 
 # Last step of cleanup
-rm /tmp/RealPortageHomepageIssues*
+# rm /tmp/RealPortageHomepageIssues*
+# rm /tmp/PortageHome*
 
 
 
 echo
 echo
 echo 
-echo "Now you can file bug reports: http://bugs.gentoo.org/enter_bug.cgi?product=Gentoo%20Linux&format=guided"
+echo "5) Follow-up..."
+echo "     Now you can file bug reports: http://bugs.gentoo.org/enter_bug.cgi?product=Gentoo%20Linux&format=guided"
 echo 
 echo
-echo "Select \"Applications\"  and enter \"Invalid HOMEPAGE for [package name]\"."
+echo "     Select \"Applications\"  and enter \"Invalid HOMEPAGE for [package name]\"."
 echo
-echo "Mention something like: \"The HOMEPAGE will not load because [what type of error].  The HOMEPAGE is:  [URL].\"" 
+echo "     Mention something like: \"The HOMEPAGE will not load because [what type of error].  The HOMEPAGE is:  [URL].\"" 
 echo
 echo
